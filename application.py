dc09d95f97cae1f915ebfcad95446a7ad6f5da74
markjoeljimenez <markjoeljimenez@gmail.com>
2020-11-04 22:49:19 -0500

    Fix upload

--- application.py
+++ application.py
@@ -1,4 +1,6 @@
 from os import environ
+import csv
+import io
 import json
 import re
 import requests
@@ -12,12 +14,12 @@ from pydfs_lineup_optimizer import get_optimizer, Site, Sport, Player, LineupOpt
 from pydfs_lineup_optimizer.constants import PlayerRank
 from draft_kings.data import SPORT_ID_TO_SPORT
 from draft_kings.client import contests, available_players, draftables, draft_group_details, sports
-from utils import SPORT_ID_TO_PYDFS_SPORT, transform_player, merge_two_dicts, generate_csv, get_positions
+from utils import SPORT_ID_TO_PYDFS_SPORT, transform_player, merge_two_dicts, generate_csv, get_positions, generate_csv_from_csv
 
 application = Flask(__name__)
 application.debug = True
-application.config["SECRET_KEY"] = environ.get('SECRET_KEY')
-# application.config["SESSION_TYPE"] = 'filesystem'
+application.config["SECRET_KEY"] = environ.get("SECRET_KEY")
+# application.config["SESSION_TYPE"] = "filesystem"
 application.config["SESSION_COOKIE_HTTPONLY"] = False
 application.config["SESSION_COOKIE_SAMESITE"] = None
 
@@ -27,7 +29,7 @@ CORS(application, supports_credentials=True)
 @application.route("/")
 def get_sports():
     response = list(map(
-        (lambda sport: {**sport, 'supported': sport["sportId"] in SPORT_ID_TO_PYDFS_SPORT}), sports()['sports']))
+        (lambda sport: {**sport, "supported": sport["sportId"] in SPORT_ID_TO_PYDFS_SPORT}), sports()["sports"]))
 
     return json.dumps(response)
 
@@ -37,7 +39,7 @@ def get_contests():
     json = request.get_json()
 
     if json:
-        sport = json.get('sport')
+        sport = json.get("sport")
 
         if sport:
             return jsonpickle.encode(contests(sport=SPORT_ID_TO_SPORT[sport]))
@@ -69,14 +71,14 @@ def get_players():
         })
 
     if request.files:
-        df = pd.read_csv(request.files.get('csv'))
+        df = pd.read_csv(request.files.get("csv"))
 
         return json.dumps({
             "players": [{
                 "id": player["ID"],
                 # "draftable_id": player["draftable_id"],
-                "first_name": player["Name"],
-                # "last_name": player["last_name"],
+                "first_name": player["Name"].split()[0],
+                "last_name": player["Name"].split()[1] if len(player["Name"].split()) > 1 else "",
                 "position": player["Position"],
                 "team": player["TeamAbbrev"],
                 "salary": player["Salary"],
@@ -92,16 +94,14 @@ def get_players():
 def optimize():
     json = request.get_json()
 
-    import_type = json.get('import_type')
-    generations = json.get('generations')
-    lockedPlayers = json.get('lockedPlayers')
-    players = json.get('players')
-    rules = json.get('rules')
-    session["sport"] = json.get('sport')
-    session["draftGroupId"] = json.get('draftGroupId')
+    lockedPlayers = json.get("lockedPlayers")
+    players = json.get("players")
+    rules = json.get("rules")
+    session["sport"] = SPORT_ID_TO_PYDFS_SPORT[json.get("sport")]
+    session["draftGroupId"] = json.get("draftGroupId")
 
     optimizer = get_optimizer(
-        Site.DRAFTKINGS, SPORT_ID_TO_PYDFS_SPORT[session.get('sport')])
+        Site.DRAFTKINGS, session.get("sport")["sport"])
     optimizer.load_players([transform_player(player)
                             for player in players])
     # optimizer.load_players_from_csv("DKSalaries-nfl-sept-5.csv")
@@ -112,15 +112,15 @@ def optimize():
     }
 
     if "NUMBER_OF_PLAYERS_FROM_SAME_TEAM" in rules:
-        for team in rules['NUMBER_OF_PLAYERS_FROM_SAME_TEAM']:
+        for team in rules["NUMBER_OF_PLAYERS_FROM_SAME_TEAM"]:
             optimizer.set_players_from_one_team({
-                team['key']: team['value']
+                team["key"]: team["value"]
             })
 
     if "NUMBER_OF_SPECIFIC_POSITIONS" in rules:
-        for position in rules['NUMBER_OF_SPECIFIC_POSITIONS']:
+        for position in rules["NUMBER_OF_SPECIFIC_POSITIONS"]:
             optimizer.set_players_with_same_position({
-                position['key']: position['value']
+                position["key"]: position["value"]
             })
 
     if "MINIMUM_SALARY_CAP" in rules:
@@ -139,7 +139,7 @@ def optimize():
             optimizer.add_player_to_lineup(optimizer.get_player_by_id(player))
 
     try:
-        optimize = optimizer.optimize(generations)
+        optimize = optimizer.optimize(rules["NUMBER_OF_GENERATIONS"])
 
         exporter = JSONLineupExporter(optimize)
         exported_json = exporter.export()
@@ -147,7 +147,7 @@ def optimize():
         session["lineups"] = exported_json["lineups"]
 
         # csv_exporter = DraftKingsCSVLineupExporter(optimize)
-        # csv_exporter.export('result.csv', lambda player: player.id)
+        # csv_exporter.export("result.csv", lambda player: player.id)
 
         return merge_two_dicts(exported_json, response)
     except LineupOptimizerException as exception:
@@ -156,23 +156,24 @@ def optimize():
         return response
 
 
-@application.route('/export')
+@application.route("/export")
 def exportCSV():
     print(session)
 
     if "lineups" in session:
         lineups = session.get("lineups")
-        draftGroupId = session.get("draftGroupId")
+        # draftGroupId = session.get("draftGroupId")
         sport = session.get("sport")
 
-        csv = generate_csv(lineups, draftGroupId, sport)
+        # csv = generate_csv(lineups, draftGroupId, sport)
+        csv = generate_csv_from_csv(lineups, sport)
 
         # response = make_response(csv)
         # response.headers["Content-Disposition"] = "attachment; filename=DKSalaries.csv"
         # response.headers["Content-type"] = "text/csv"
 
         return Response(csv,
-                        mimetype='text/csv',
+                        mimetype="text/csv",
                         headers={"Content-disposition":
                                  "attachment; filename=DKSalaries.csv"})
 
@@ -182,17 +183,17 @@ def exportCSV():
 # @ application.route("/stats")
 # def stats():
 #     playerId = players.find_players_by_full_name(
-#         request.args.get('player'))[0].get('id', None)
+#         request.args.get("player"))[0].get("id", None)
 
 #     player_info = json.loads(commonplayerinfo.CommonPlayerInfo(
-#         player_id=playerId).get_normalized_json()).get('CommonPlayerInfo', None)[0]
+#         player_id=playerId).get_normalized_json()).get("CommonPlayerInfo", None)[0]
 #     player_headline_stats = json.loads(commonplayerinfo.CommonPlayerInfo(
-#         player_id=playerId).get_normalized_json()).get('PlayerHeadlineStats', None)[0]
+#         player_id=playerId).get_normalized_json()).get("PlayerHeadlineStats", None)[0]
 
 #     player_stats = json.loads(playerprofilev2.PlayerProfileV2(
 #         per_mode36="PerGame", player_id=playerId).get_normalized_json())
 
-#     teamId = player_info.get('TEAM_ID', None)
+#     teamId = player_info.get("TEAM_ID", None)
 
 #     profile_picture = "https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/%s/2019/260x190/%s.png" % (
 #         teamId, playerId)
--- utils.py
+++ utils.py
@@ -5,24 +5,50 @@ import pandas as pd
 from pydfs_lineup_optimizer import Player, Sport
 from pydfs_lineup_optimizer.constants import PlayerRank
 from draft_kings.client import draftables
-from positions import Positions
 
 SPORT_ID_TO_PYDFS_SPORT = {
-    1: Sport.FOOTBALL,
-    2: Sport.BASEBALL,
-    3: Sport.HOCKEY,
-    4: Sport.BASKETBALL,
-    # 6: Sport.BASKETBALL,  # Sport.COLLEGE_BASKETBALL
-    # 5: Sport.FOOTBALL,  # Sport.COLLEGE_FOOTBALL
-    9: Sport.MMA,
-    10: Sport.NASCAR,
-    11: Sport.LEAGUE_OF_LEGENDS,
-    12: Sport.SOCCER,
-    13: Sport.GOLF,
-    14: Sport.CANADIAN_FOOTBALL,
-    # 15: Sport.BASKETBALL,  # Sport.EUROLEAGUE_BASKETBALL
-    16: Sport.TENNIS,
-    # : Sport.ARENA_FOOTBALL_LEAGUE,
+    1: {
+        "sport": Sport.FOOTBALL,
+        "positions": ["QB", "RB", "RB", "WR", "WR", "WR", "TE", "FLEX", "DST"]
+    },
+    2: {
+        "sport": Sport.BASEBALL,
+        "positions": ["P", "P", "C", "1B", "2B", "3B", "SS", "OF", "OF", "OF"]
+    },
+    3: {
+        "sport": Sport.HOCKEY,
+        "positions": ["C", "C", "W", "W", "W", "D", "D", "G", "UTIL"]
+    },
+    4: {
+        "sport": Sport.BASKETBALL,
+        "positions": ["PG", "SG", "SF", "PF", "C", "G", "F", "UTIL"]
+    },
+    # # 6: Sport.BASKETBALL,  # Sport.COLLEGE_BASKETBALL
+    # # 5: Sport.FOOTBALL,  # Sport.COLLEGE_FOOTBALL
+    9: {
+        "sport": Sport.MMA,
+        "positions": ["F", "F", "F", "F", "F", "F"]
+    },
+    10: {
+        "sport": Sport.NASCAR,
+        "positions": ["D", "D", "D", "D", "D", "D"]
+    },
+    # 11: Sport.LEAGUE_OF_LEGENDS,
+    12: {
+        "sport": Sport.SOCCER,
+        "positions": ["F", "F", "M", "M", "D", "D", "GK", "UTIL"]
+    },
+    13:  {
+        "sport": Sport.GOLF,
+        "positions": ["G", "G", "G", "G", "G", "G"]
+    },
+    # 14: Sport.CANADIAN_FOOTBALL,
+    # # 15: Sport.BASKETBALL,  # Sport.EUROLEAGUE_BASKETBALL
+    16: {
+        "sport": Sport.TENNIS,
+        "positions": ["P", "P", "P", "P", "P", "P"]
+    },
+    # # : Sport.ARENA_FOOTBALL_LEAGUE,
 }
 
 
@@ -39,13 +65,35 @@ def transform_player(player):
         player["last_name"],
         player["position"].split("/"),
         player["team"],
-        player["salary"],
-        player["points_per_contest"],
-        True if player["status"] == "O" else False,
+        float(player["salary"]),
+        float(player["points_per_contest"]),
+        player.get("status") == "O",
         PlayerRank.REGULAR,
         None,
-        player["min_exposure"] if "min_exposure" in player else None,
-        player["projected_ownership"] if "projected_ownership" in player else None
+        player.get("min_exposure"),
+        player.get("projected_ownership")
+    )
+
+    return player
+
+
+def transform_player_from_csv(player):
+    player = Player(
+        player["ID"],
+        player['Name'].split()[0],
+        player['Name'].split()[1] if len(player['Name'].split()) > 1 else '',
+        player["Position"].split("/"),
+        player["TeamAbbrev"],
+        float(player["Salary"]),
+        float(player["AvgPointsPerGame"]),
+        # True if player["status"] == "O" else False,
+        False,
+        PlayerRank.REGULAR,
+        None,
+        # player["min_exposure"] if "min_exposure" in player else None,
+        # player["projected_ownership"] if "projected_ownership" in player else None
+        None,
+        None
     )
 
     return player
@@ -84,9 +132,23 @@ def generate_csv(lineups, draft_group_id, sport):
 
     for index, lineup in enumerate(lineups):
         if index == 0:
-            header = [pos for pos in positions]
+            header = [pos for pos in sport["positions"]]
             lineup_writer.writerow(header)
         row = [get_draftable_id(player) for player in lineup["players"]]
+        lineup_writer.writerow(row)
+
+    return csvfile.getvalue()
+
+
+def generate_csv_from_csv(lineups, sport):
+    csvfile = io.StringIO()
+    lineup_writer = csv.writer(csvfile, delimiter=',')
+
+    for index, lineup in enumerate(lineups):
+        if index == 0:
+            header = [pos for pos in sport["positions"]]
+            lineup_writer.writerow(header)
+        row = lineup["players"]
         lineup_writer.writerow(row)
 
     return csvfile.getvalue()
